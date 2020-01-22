209ee06b8bf2a4dd0f592be7d737fab5059ffcc6
Victor Hugo <victor.morais@tvglobo.com.br>
2020-01-22 12:11:50 -0300

    changed github account

---
+++ js/new-rm.js
@@ -0,0 +1,47 @@
+var app = new PIXI.Application({
+  width: 300,
+  height: 250,
+  backgroundColor: 0xc1c1c1
+})
+document.body.appendChild(app.view);
+
+var background = PIXI.Sprite.from("../images/new-ifoods-rm.png")
+var raspar = PIXI.Sprite.from("../images/new-raspar-rm.png")
+app.stage.addChild(background)
+app.stage.addChild(raspar)
+raspar.anchor.set(0.5);
+raspar.position.x = 150;
+raspar.position.y = 160;
+var renderTexture = PIXI.RenderTexture.create(app.screen.width, app.screen.height);
+var renderTextureSprite = new PIXI.Sprite(renderTexture);
+
+app.stage.addChild(renderTextureSprite);
+raspar.mask = renderTextureSprite;
+
+var brush = new PIXI.Graphics();
+brush.beginFill(0xffffff);
+brush.drawCircle(0, 0, 25);
+brush.endFill();
+
+app.stage.interactive = true;
+app.stage.on('pointerdown', pointerDown);
+app.stage.on('pointerup', pointerUp);
+app.stage.on('pointermove', pointerMove);
+
+var dragging = false;
+
+function pointerMove(event) {
+    if (dragging) {
+        brush.position.copyFrom(event.data.global);
+        app.renderer.render(brush, renderTexture, false, null, false);
+    }
+}
+
+function pointerDown(event) {
+    dragging = true;
+    pointerMove(event);
+}
+
+function pointerUp(event) {
+    dragging = false;
+}
