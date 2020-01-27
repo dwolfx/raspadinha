622ca58a5cd1281656afe8baea6c56a1ba4f3e2f
Victor Hugo <victor.morais@tvglobo.com.br>
2020-01-27 19:30:55 -0300

    Add full functionality to raspadinha

--- js/new-rm.js
+++ js/new-rm.js
@@ -1,9 +1,3 @@
-var app = new PIXI.Application({
-  width: 300,
-  height: 250,
-  backgroundColor: 0xff8000
-})
-document.body.appendChild(app.view);
 
 var WIDTH = 270;
 var HEIGHT = 110;
@@ -11,22 +5,31 @@ var TOP_X = 150;
 var TOP_Y = 160;
 var BYTES_PER_PIXEL = 4;
 var REVEAL_PERCENTAGE = 75;
-var background = PIXI.Sprite.from("../images/new-ifoods-rm.png")
-var scratch = PIXI.Sprite.from("../images/new-raspar-rm.png")
+var RADIUS = 20;
+var URL = 'https://g1.globo.com';
 
-app.stage.addChild(background)
-app.stage.addChild(scratch)
+var dragging = true;
+
+var app = new PIXI.Application({
+  width: 300,
+  height: 250,
+  backgroundColor: 0xff8000
+});
+document.body.appendChild(app.view);
+
+var background = PIXI.Sprite.from('../images/new-ifoods-rm.png');
+
+app.stage.addChild(background);
+
+var scratch = PIXI.Sprite.from('../images/new-raspar-rm.png');
+
+app.stage.addChild(scratch);
 scratch.anchor.set(0.5);
 scratch.position.x = TOP_X;
 scratch.position.y = TOP_Y;
+
 var renderTexture = PIXI.RenderTexture.create(WIDTH, HEIGHT);
 var renderTextureSprite = new PIXI.Sprite(renderTexture);
-
-var posX
-var posY
-var radius = 20;
-var porc = 0
-var porcAtual
 
 app.stage.addChild(renderTextureSprite);
 renderTextureSprite.anchor.set(0.5);
@@ -36,81 +39,53 @@ scratch.mask = renderTextureSprite;
 
 var brush = new PIXI.Graphics();
 brush.beginFill(0xffffff);
-brush.drawCircle(0, 0, radius);
+brush.drawCircle(0, 0, RADIUS);
 brush.endFill();
 
 app.stage.interactive = true;
-app.stage.on('pointerover', pointerDown);
-app.stage.on('pointerdown', pointerDown);
-app.stage.on('pointerup', pointerUp);
 app.stage.on('pointermove', pointerMove);
-app.stage.on('pointerout', pointerOut);
-app.stage.on('pointeroutside', pointerOut);
 
-var dragging = false;
 function pointerMove(event) {
-    if (dragging) {
-        var pos = event.data.getLocalPosition(renderTextureSprite);
-        var x = (pos.x + TOP_X) - radius/2;
-        var y = (pos.y + TOP_Y/2) - radius;
-        
-        brush.position.copyFrom({ x: x, y: y });
+  if (!dragging) return;
 
-        app.renderer.render(brush, renderTexture, false, null, false);
+  var pos = event.data.getLocalPosition(renderTextureSprite);
+  var x = (pos.x + TOP_X) - RADIUS / 2;
+  var y = (pos.y + TOP_Y / 2) - RADIUS;
 
-        posX = Math.round(event.data.global.x)
-        posY = Math.round(event.data.global.y)
-        if(getPaintedPercentage(renderTexture) >= REVEAL_PERCENTAGE){
-            reveal()
-        }
-        
-    }
-}
+  brush.position.copyFrom({ x: x, y: y });
 
-function getPaintedPercentage(renderTexture) {    
-    var pixels = app.renderer.plugins.extract.pixels(renderTexture);
-    var total = pixels.length / BYTES_PER_PIXEL; 
-    var sum = 0;
+  app.renderer.render(brush, renderTexture, false, null, false);
 
-    for (var i = 0; i < pixels.length; i += BYTES_PER_PIXEL) {
-        if (pixels[i+3] === 255) {
-            sum++;
-        }
-    }
-
-    return Math.round(sum * 100 / total);
-}
-
-function debounce(func, wait, immediate) {
-    var timeout;
-    
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
-
-function pointerDown(event) {
-    dragging = true;
-    pointerMove(event);
-    console.log(dragging);    
-}
-
-function reveal(){
-    console.log('entrou no reveal')
-}
-
-function pointerUp(event) {
+  if (getPaintedPercentage(renderTexture) >= REVEAL_PERCENTAGE) {
     dragging = false;
+    reveal(renderTexture);
+  }
 }
 
-function pointerOut(event){
-    reveal();
+function getPaintedPercentage(renderTexture) {
+  var pixels = app.renderer.plugins.extract.pixels(renderTexture);
+  var total = pixels.length / BYTES_PER_PIXEL;
+  var sum = 0;
+
+  for (var i = 0; i < pixels.length; i += BYTES_PER_PIXEL) {
+    if (pixels[i + 3] === 255) {
+      sum++;
+    }
+  }
+
+  return Math.round(sum * 100 / total);
+}
+
+function reveal(renderTexture) {
+  var rect = new PIXI.Graphics();
+
+  rect.beginFill(0xffffff);
+  rect.drawRect(0, 0, app.screen.width, app.screen.height);
+  rect.endFill();
+  
+  app.renderer.render(rect, renderTexture, false, null, false);
+
+  app.stage.on('pointerdown', function() {
+    window.open(URL, '_blank');
+  });
 }
