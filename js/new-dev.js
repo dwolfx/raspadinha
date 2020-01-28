45ea2066fc59f4daa52330977f200d9ab773ae73
Victor Hugo <victor.morais@tvglobo.com.br>
2020-01-28 15:17:23 -0300

    actualized halfpage

--- js/new-dev.js
+++ js/new-dev.js
@@ -1,52 +1,102 @@
+
+var WIDTH = 200;
+var HEIGHT = 270;
+var TOP_X = 150;
+var TOP_Y = 350;
+var BYTES_PER_PIXEL = 4;
+var REVEAL_PERCENTAGE = 60;
+var RADIUS = 20;
+var URL = 'https://g1.globo.com';
+
+var dragging = true;
+
 var app = new PIXI.Application({
   width: 300,
   height: 600,
   backgroundColor: 0xc1c1c1
-})
+});
 document.body.appendChild(app.view);
 
-var background = PIXI.Sprite.from("../images/bg-ifoods.png")
-var raspar = PIXI.Sprite.from("../images/raspar.png")
-app.stage.addChild(background)
-app.stage.addChild(raspar)
-raspar.anchor.set(0.5);
-raspar.position.x = 150;
-raspar.position.y = 350;
-var renderTexture = PIXI.RenderTexture.create(app.screen.width, app.screen.height);
+var background = PIXI.Sprite.from('../images/bg-ifoods.png');
+
+app.stage.addChild(background);
+
+var scratch = PIXI.Sprite.from('../images/raspar.png');
+
+app.stage.addChild(scratch);
+scratch.anchor.set(0.5);
+scratch.position.x = TOP_X;
+scratch.position.y = TOP_Y;
+
+var renderTexture = PIXI.RenderTexture.create(WIDTH, HEIGHT);
 var renderTextureSprite = new PIXI.Sprite(renderTexture);
 
 app.stage.addChild(renderTextureSprite);
-raspar.mask = renderTextureSprite;
-
-console.log(renderTexture)
+renderTextureSprite.anchor.set(0.5);
+renderTextureSprite.position.x = TOP_X;
+renderTextureSprite.position.y = TOP_Y;
+scratch.mask = renderTextureSprite;
 
 var brush = new PIXI.Graphics();
-brush.beginFill(0xbbffff);
-brush.drawCircle(0, 0, 30);
+brush.beginFill(0xffffff);
+brush.drawCircle(0, 0, RADIUS);
 brush.endFill();
 
 app.stage.interactive = true;
-app.stage.on('pointerdown', pointerDown);
-app.stage.on('pointerup', pointerUp);
 app.stage.on('pointermove', pointerMove);
 
-var dragging = false;
-
 function pointerMove(event) {
-  if (dragging) {
-    console.log(event.data.global)
-    brush.position.copyFrom(event.data.global);
-    app.renderer.render(brush, renderTexture, false, null, false);
+  if (!dragging) return;
+
+  var pos = event.data.getLocalPosition(renderTextureSprite);
+  var x = (pos.x + TOP_X) - RADIUS * 2.5;
+  var y = (pos.y + TOP_Y / 2) - RADIUS * 2;
+
+  brush.position.copyFrom({ x: x, y: y });
+
+  app.renderer.render(brush, renderTexture, false, null, false);
+
+  if (getPaintedPercentage(renderTexture) >= REVEAL_PERCENTAGE) {
+    dragging = false;
+    reveal(renderTexture);
   }
 }
 
-function pointerDown(event) {
-  console.log('clicou')
-    dragging = true;
-    pointerMove(event);
+function getPaintedPercentage(renderTexture) {
+  var pixels = app.renderer.plugins.extract.pixels(renderTexture);
+  var total = pixels.length / BYTES_PER_PIXEL;
+  var sum = 0;
+
+  for (var i = 0; i < pixels.length; i += BYTES_PER_PIXEL) {
+    if (pixels[i + 3] === 255) {
+      sum++;
+    }
+  }
+
+  return Math.round(sum * 100 / total);
 }
 
-function pointerUp(event) {
-  console.log('soltou')
-  dragging = false;
+function reveal(renderTexture) {
+  var rect = new PIXI.Graphics();
+
+  rect.beginFill(0xffffff);
+  rect.drawRect(0, 0, app.screen.width, app.screen.height);
+  rect.endFill();
+  
+  app.renderer.render(rect, renderTexture, false, null, false);
+
+  var click = document.createElement('a');
+  click.href = URL;
+  click.target = '_blank';
+  
+  var div = document.createElement('div');
+  div.style.width = '100vw';
+  div.style.height = '100vh';
+  div.style.position = 'fixed';
+  div.style.top = '0';
+  div.style.left = '0';
+
+  click.appendChild(div);
+  document.body.appendChild(click);
+  
 }
